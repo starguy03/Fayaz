bc0fcb92e5830d85270451376ffc6973d7bfdfab
starguy03 <104380451+starguy03@users.noreply.github.com>
2022-05-30 00:13:58 +0530

    Update Orange.java

--- GITLAB/src/Orange.java
+++ GITLAB/src/Orange.java
@@ -1,4 +1,8 @@
-	public class Orange {
+	
+//RBCA-20-142
+package journalwork;
+
+public class Apple {
 	String color;
 	Orange(String color)
 	{
@@ -10,9 +14,11 @@
 	}
 	
 	public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	Orange greenApple = new Orange("green");
-	String color = greenApple.getColor();
-	System.out.println("color of apple is " + color);
-	}
-	}
+	        // TODO Auto-generated method stub
+	        Apple A1 = new Apple("Red");
+	        String color = A1.getColor();
+	        System.out.println("color of apple is " + color);
+	
+       }
+	
+}
